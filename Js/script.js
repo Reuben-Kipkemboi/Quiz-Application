3690b41d5f539390354146c90eab4b50a96ffc3e
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-02-05 17:29:58 +0300

    bg:submit-buttton - Onclick cant display on screen

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,7 @@
 function submitAnswer() {
-    alert("Hello World!");
+    // alert("Hello World!");
+    var score = 0;
+    var currentAnswer = document.quiz-form.question-one.value;
+    if (currentAnswer==1){score+=20};
+    console.log(score);
   }
