ca9fb7aba9f3162ebf10c260b4949806dcb3617a
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-02-06 07:13:59 +0300

    ts:Submit-buton - Submit button test on a click event

--- Js/script.js
+++ Js/script.js
@@ -1,7 +1,30 @@
+// function submitAnswer() {
+//     // alert("Hello World!");
+//     var score = 0; //Declaring variable score i.e the original score.
+//     var myAnswer1 = document.quiz-form.question1.value;  //tracks the current answer to question one.
+//     var myAnswer2 = document.quiz-form.question2.value;
+//     var myAnswer3 = document.quiz-form.question3.value;
+//     var myAnswer4 = document.quiz-form.question4.value;
+//     var myAnswer5 = document.quiz-form.question5.value;
+
+
+//     if (myAnswer1===1){score += 20};   //If statement that adds the points from the current score + 20.
+//     if (myAnswer2==="Class"){score += 20}; 
+//     if (myAnswer3==="1995"){score += 20}; 
+//     if (myAnswer4==="script"){score += 20}; 
+//     if (myAnswer5==="For"){score += 20}; 
+//     document.write(score);    //prints on the screen the output of the score. 
+//   }
+
+
+//  code 2 for javascript
 function submitAnswer() {
-    // alert("Hello World!");
-    var score = 0;
-    var currentAnswer = document.quiz-form.question-one.value;
-    if (currentAnswer==1){score+=20};
-    console.log(score);
-  }
+  alert("Hello and welcome to todays Quiz");
+}
+// var rightAnswers = {
+//   question1 : "1",
+//   question2 : "Class",
+//   question3 : "1995",
+//   question4 : "Script",
+//   question5 : "For"
+// };
